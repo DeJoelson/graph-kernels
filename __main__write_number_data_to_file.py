53d75955dc025c06d65d334591ba037a20d5c30d
DeJoelson <webmaster1234567890@gmail.com>
2018-06-21 01:31:26 -0600

    Added Header to File

--- __main__write_number_data_to_file.py
+++ __main__write_number_data_to_file.py
@@ -9,18 +9,20 @@ from constants import OUTPUT_DIR, RANDOM_SEED
 from metrics.evaluator import Evaluator
 import datetime
 
-import data.T5_Adj as dta
+import data.T6_Adj_FAST_Unlabled as dta
 
 if __name__ == '__main__':
     
-    data = dta.get_T5_Adj()
-    data_format = "AdjacencyMatrix"
-    TOrder = "T5"
+    data = dta.get_T6_Adj_FAST_Unlabled()
+    data_format = "AdjacencyMatrix-Unlabeled"
+    TOrder = "T6"
+    is_FAST = True
 
     X = []
     y = [] 
 
     random.seed(RANDOM_SEED)
+    #random.seed(2)
     random.shuffle(data)
 
     for item in data:
@@ -32,6 +34,17 @@ if __name__ == '__main__':
 
     total_observations = len(X)
     iterations = min(50, int(total_observations/5))
+    
+    file_suffix = ""
+    
+    if is_FAST:
+        file_suffix = "-FAST"
+        
+    filename = OUTPUT_DIR+TOrder+"-"+data_format+file_suffix+"-Numbers.csv"
+    
+    with open(filename, "a") as f:
+        header = "PredictorSet, Method, TotalObservations, NumberOfTrainingObservations, FitTime0, FitTime1, FitTime2, FitTime3, FitTime4, FitTime5, FitTime6, FitTime7, FitTime8, FitTime9, ScoreTime0, ScoreTime1, ScoreTime2, ScoreTime3, ScoreTime4, ScoreTime5, ScoreTime6, ScoreTime7, ScoreTime8, ScoreTime9, TrainAccuracy0, TrainAccuracy1, TrainAccuracy2, TrainAccuracy3, TrainAccuracy4, TrainAccuracy5, TrainAccuracy6, TrainAccuracy7, TrainAccuracy8, TrainAccuracy9, TestAccuracy0, TestAccuracy1, TestAccuracy2, TestAccuracy3, TestAccuracy4, TestAccuracy5, TestAccuracy6, TestAccuracy7, TestAccuracy8, TestAccuracy9"
+        f.write(header + "\n")
 
     for i in range(0, iterations):  #Increment by 5 observations
         number_of_training_observations = i*5+5
@@ -39,7 +52,7 @@ if __name__ == '__main__':
         print(i, "Of ", iterations)
 
         evaluator_device = Evaluator(X, y)
-        evaluator_device.cross_validate_to_file_number_based(file_name=OUTPUT_DIR+TOrder+"-"+data_format+"-Numbers.csv", predictor_set_name=TOrder+"-"+data_format,training_observations=number_of_training_observations)
+        evaluator_device.cross_validate_to_file_number_based(file_name=filename, predictor_set_name=TOrder+"-"+data_format,training_observations=number_of_training_observations)
 
         print("Total Time Last: ", datetime.datetime.now()-endTime)
         endTime = datetime.datetime.now()
--- __main__write_ratio_data_to_file.py
+++ __main__write_ratio_data_to_file.py
@@ -9,18 +9,20 @@ from constants import OUTPUT_DIR, RANDOM_SEED
 from metrics.evaluator import Evaluator
 import datetime
 
-import data.T4_Lap as dta
+import data.T6_Adj_FAST_Unlabled as dta
 
 if __name__ == '__main__':
     
-    data = dta.get_T4_Lap()
-    data_format = "LaplacianMatrix"
-    TOrder = "T4"
+    data = dta.get_T6_Adj_FAST_Unlabled()
+    data_format = "AdjacencyMatrix-Unlabeled"
+    TOrder = "T6"
+    is_FAST = True
 
     X = []
     y = [] 
     
     random.seed(RANDOM_SEED)
+    #random.seed(2)
     random.shuffle(data)
     
     for item in data:
@@ -30,12 +32,23 @@ if __name__ == '__main__':
     startTime = datetime.datetime.now()
     endTime = datetime.datetime.now()
 
+    file_suffix = ""
+        
+    if is_FAST:
+        file_suffix = "-FAST"
+        
+    filename = OUTPUT_DIR+TOrder+"-"+data_format+file_suffix+"-Ratio.csv"
+    
+    with open(filename, "a") as f:
+        header = "PredictorSet, Method, TotalObservations, TestingTrainingSeperationRatio, FitTime0, FitTime1, FitTime2, FitTime3, FitTime4, FitTime5, FitTime6, FitTime7, FitTime8, FitTime9, ScoreTime0, ScoreTime1, ScoreTime2, ScoreTime3, ScoreTime4, ScoreTime5, ScoreTime6, ScoreTime7, ScoreTime8, ScoreTime9, TrainAccuracy0, TrainAccuracy1, TrainAccuracy2, TrainAccuracy3, TrainAccuracy4, TrainAccuracy5, TrainAccuracy6, TrainAccuracy7, TrainAccuracy8, TrainAccuracy9, TestAccuracy0, TestAccuracy1, TestAccuracy2, TestAccuracy3, TestAccuracy4, TestAccuracy5, TestAccuracy6, TestAccuracy7, TestAccuracy8, TestAccuracy9"
+        f.write(header + "\n") 
+
     for i in range(0,17):
         test_size_ratio = 1 - ((i*5+10)/100)
         print(i, "Of 17")
 
         evaluator_device = Evaluator(X, y)
-        evaluator_device.cross_validate_to_file(file_name=OUTPUT_DIR+TOrder+"-"+data_format+"-Ratio.csv", predictor_set_name=TOrder+"-"+data_format,test_size_ratio=test_size_ratio)
+        evaluator_device.cross_validate_to_file(file_name=filename, predictor_set_name=TOrder+"-"+data_format,test_size_ratio=test_size_ratio)
 
         print("Total Time Last: ", datetime.datetime.now()-endTime)
         endTime = datetime.datetime.now()
